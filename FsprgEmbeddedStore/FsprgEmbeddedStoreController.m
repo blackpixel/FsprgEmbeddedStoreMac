12648d9f42abd7d9c62e07b81b871223094b1080
Julieta <julieta@blackpixel.com>
2016-07-22 11:17:08 -0300

    Remove warnings

--- FsprgEmbeddedStore/FsprgEmbeddedStoreController.m
+++ FsprgEmbeddedStore/FsprgEmbeddedStoreController.m
@@ -14,7 +14,7 @@
 #define RETRIEVE_SSL_CERTIFICATES defined(MAC_OS_X_VERSION_10_6) && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6
 
 
-@interface FsprgEmbeddedStoreController (Private)
+@interface FsprgEmbeddedStoreController (Private) <WebFrameLoadDelegate, WebUIDelegate, WebResourceLoadDelegate>
 
 - (void)setIsLoading:(BOOL)aFlag;
 - (void)setEstimatedLoadingProgress:(double)aProgress;
